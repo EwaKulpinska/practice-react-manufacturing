1e11f8e11b7cb58fedf48773048005722063c01f
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-07-30 10:00:47 +0200

    Added a link to my Trello board

---
+++ 01/Trello.js
@@ -0,0 +1,9 @@
+export const Trello = () => {
+    return (
+        <div>
+            <a href={'https://trello.com/invite/b/UngLIgrk/ATTI847b6566a11845a8ee38cd01201fb6cb97D1C819/as2023'} target={'blank'}>MY TRELLO BOARD</a>
+        </div>
+    )
+}
+
+export default Trello
